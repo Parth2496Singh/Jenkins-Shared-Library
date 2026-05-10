d179b6aca62e34bfcd717b55a88157aefb311f91
Parth Singh Kushwaha <156062440+Parth2496Singh@users.noreply.github.com>
2026-05-11 00:30:33 +0530

    Add DockerHub push functionality in Groovy script for Jenkins Shared Library

---
+++ vars/dockerhub_push.groovy
@@ -0,0 +1,7 @@
+def call(String DockerImage, String ImageTag, String dockerhubuser){
+  withCredentials([usernamePassword('credentialsId':"dockerHubCred",passwordVariable:"dockerHubPass", usernameVariable:"dockerHubUser")]){
+  sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass} "
+  sh "docker image tag ${DockerImage}:${ImageTag} ${dockerhubuser}/${DockerImage}:${ImageTag}"
+  sh "docker push ${dockerhubuser}/${DockerImage}:${ImageTag}"
+  }
+  
