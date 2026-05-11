8023f676c1f370f8b16a1c81efc665335b263652
Parth Singh Kushwaha <156062440+Parth2496Singh@users.noreply.github.com>
2026-05-11 20:26:38 +0530

    Update Docker Hub push script for secure login

--- vars/dockerhub_push.groovy
+++ vars/dockerhub_push.groovy
@@ -1,9 +1,13 @@
 def call(String DockerImage, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword('credentialsId':"dockerHubCred",passwordVariable:"dockerHubPass", usernameVariable:"dockerHubUser")]){
-  sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass} "
-  sh "docker image tag ${DockerImage}:${ImageTag} ${dockerhubuser}/${DockerImage}:${ImageTag}"
-  sh "docker push ${dockerhubuser}/${DockerImage}:${ImageTag}"
+  withCredentials([usernamePassword(credentialsId: "dockerHubCred", passwordVariable: "dockerHubPass", usernameVariable: "dockerHubUser")]){
+    // Use the more secure stdin login
+    sh "echo ${env.dockerHubPass} | docker login -u ${env.dockerHubUser} --password-stdin"
+    
+    // Tag the local image for the remote repo
+    sh "docker image tag ${DockerImage}:${ImageTag} ${dockerhubuser}/${DockerImage}:${ImageTag}"
+    
+    // Push the remote-tagged image
+    sh "docker push ${dockerhubuser}/${DockerImage}:${ImageTag}"
   }
 }
-  
   
