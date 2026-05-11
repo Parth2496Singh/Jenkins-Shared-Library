26e745b643877821f2a6372ed2dc4620d0aa2167
Parth Singh Kushwaha <156062440+Parth2496Singh@users.noreply.github.com>
2026-05-11 20:26:04 +0530

    Remove dockerHubUser parameter from build function

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,3 @@
-def call(String DockerImage, String ImageTag, String dockerHubUser){
-  sh "docker build -t ${dockerHubUser}/${DockerImage}:${ImageTag} ."
-}
-                                           
+def call(String DockerImage, String ImageTag){
+  sh "docker build -t ${DockerImage}:${ImageTag} ."
+}                              
